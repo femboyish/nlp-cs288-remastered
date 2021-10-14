7e13a7e22def15fbdcae4cf32665c0d97b08a268
femboyish <155249604+femboyish@users.noreply.github.com>
2021-10-14 16:43:28 +0000

    Updates chapter2/01_Configuration.md

--- a1/src/edu/berkeley/nlp/assignments/assign1/student/KneserNeyTrigramLm.java
+++ a1/src/edu/berkeley/nlp/assignments/assign1/student/KneserNeyTrigramLm.java
@@ -35,4 +35,7 @@ public class KneserNeyTrigramLm implements NgramLanguageModel {
     * 0.90 -> 354.125s, 861M
     * 0.95 -> 710.900s, 836M
     */
-   public static final float loadFactor = 
+   public static final float loadFactor = 0.75f;
+   
+   /**
+    * Initial capacities for the counters and hash m
