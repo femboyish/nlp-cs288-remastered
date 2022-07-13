a69421de027dcabd9e3140f848c6b609397ababe
femboyish <155249604+femboyish@users.noreply.github.com>
2022-07-13 10:21:49 +0000

    Added a unit test

--- a1/src/edu/berkeley/nlp/assignments/assign1/student/TrigramCounter.java
+++ a1/src/edu/berkeley/nlp/assignments/assign1/student/TrigramCounter.java
@@ -2,4 +2,5 @@ package edu.berkeley.nlp.assignments.assign1.student;
 
 
 /**
- * A very memory efficient trigram counter using a modified hash ma
+ * A very memory efficient trigram counter using a modified hash map
+ * implementation. The hash map has only one long array (64bit each
