926e04c7e06637e91cd118616d1198af46d9c710
femboyish <155249604+femboyish@users.noreply.github.com>
2022-07-22 09:23:56 +0000

    center image

--- a1/src/edu/berkeley/nlp/assignments/assign1/student/TrigramCounter.java
+++ a1/src/edu/berkeley/nlp/assignments/assign1/student/TrigramCounter.java
@@ -3,4 +3,5 @@ package edu.berkeley.nlp.assignments.assign1.student;
 
 /**
  * A very memory efficient trigram counter using a modified hash map
- * implementation. The hash map has only one long array (64bit each
+ * implementation. The hash map has only one long array (64bit each element).
+ * Both the keys (trigram) and the values are packed into
