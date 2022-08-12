43b981ac64180038585e43f8b398faed23179a44
femboyish <155249604+femboyish@users.noreply.github.com>
2022-08-12 06:34:10 +0000

    v0.6.2

--- a1/src/edu/berkeley/nlp/assignments/assign1/student/Utils.java
+++ a1/src/edu/berkeley/nlp/assignments/assign1/student/Utils.java
@@ -5,4 +5,8 @@ public class Utils {
    public static final long mask = -1L >>> 32;
 
    public static long pack(int left, int right) {
-     return ((long)left << 32) | ((long)right & mas
+     return ((long)left << 32) | ((long)right & mask);
+   }
+
+   public static int left(long v) {
+     return (int
