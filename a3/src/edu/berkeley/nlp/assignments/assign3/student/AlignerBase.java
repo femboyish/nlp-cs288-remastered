f4cb3538e77e14e88dba9a0739b1dc661f269667
femboyish <155249604+femboyish@users.noreply.github.com>
2023-04-01 08:55:25 +0000

    add output

--- a3/src/edu/berkeley/nlp/assignments/assign3/student/AlignerBase.java
+++ a3/src/edu/berkeley/nlp/assignments/assign3/student/AlignerBase.java
@@ -1,4 +1,6 @@
 package edu.berkeley.nlp.assignments.assign3.student;
 
 import edu.berkeley.nlp.langmodel.EnglishWordIndexer;
-import edu.berkel
+import edu.berkeley.nlp.mt.Alignment;
+import edu.berkeley.nlp.mt.SentencePair;
+import ed
