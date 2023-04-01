14e7535f970093a1805fbceed3f9fe64f46b1172
femboyish <155249604+femboyish@users.noreply.github.com>
2023-04-01 21:52:19 +0000

    help: correct behavior for is_executable on Windows

--- a3/src/edu/berkeley/nlp/assignments/assign3/student/AlignerBase.java
+++ a3/src/edu/berkeley/nlp/assignments/assign3/student/AlignerBase.java
@@ -4,4 +4,8 @@ import edu.berkeley.nlp.langmodel.EnglishWordIndexer;
 import edu.berkeley.nlp.mt.Alignment;
 import edu.berkeley.nlp.mt.SentencePair;
 import edu.berkeley.nlp.mt.WordAligner;
-import edu.berkeley.nlp.util.Cou
+import edu.berkeley.nlp.util.CounterMap;
+import edu.berkeley.nlp.util.StringIndexer;
+
+/**
+ * Base class f
