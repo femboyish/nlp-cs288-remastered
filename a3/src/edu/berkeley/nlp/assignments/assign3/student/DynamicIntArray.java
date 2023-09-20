d6d4a0a3f60d76de40c134795a70e4b9ea4d6087
femboyish <155249604+femboyish@users.noreply.github.com>
2023-09-20 11:00:58 +0000

    Updated headers.

--- a3/src/edu/berkeley/nlp/assignments/assign3/student/DynamicIntArray.java
+++ a3/src/edu/berkeley/nlp/assignments/assign3/student/DynamicIntArray.java
@@ -1,4 +1,5 @@
 package edu.berkeley.nlp.assignments.assign3.student;
 
 /**
- * An integer (primitive data type) array that dynamically grows
+ * An integer (primitive data type) array that dynamically grows. We don't use
+ * the Java Vector class because the auto-boxing of primitives cost
