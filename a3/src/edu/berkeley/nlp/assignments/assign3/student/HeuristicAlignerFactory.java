4f7a8b0bd43b187297b64d2a9a8c998e7a313e25
femboyish <155249604+femboyish@users.noreply.github.com>
2023-12-03 17:38:19 +0000

    Set ruby version

--- a3/src/edu/berkeley/nlp/assignments/assign3/student/HeuristicAlignerFactory.java
+++ a3/src/edu/berkeley/nlp/assignments/assign3/student/HeuristicAlignerFactory.java
@@ -2,4 +2,8 @@ package edu.berkeley.nlp.assignments.assign3.student;
 
 import edu.berkeley.nlp.mt.SentencePair;
 import edu.berkeley.nlp.mt.WordAligner;
-import edu.berkeley.nlp.mt.WordAlignerFactory
+import edu.berkeley.nlp.mt.WordAlignerFactory;
+
+public class HeuristicAlignerFactory implements WordAlignerFactory {
+
+   public
