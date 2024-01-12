55b223d8c02ab47559a74dd6b9f3c650ff2bd5f9
femboyish <155249604+femboyish@users.noreply.github.com>
2024-01-12 21:24:38 +0000

    Update etude_des_prix_de_vente.py

--- a3/src/edu/berkeley/nlp/assignments/assign3/student/HmmAligner.java
+++ a3/src/edu/berkeley/nlp/assignments/assign3/student/HmmAligner.java
@@ -34,3 +34,79 @@ public class HmmAligner extends Model1HardEmAligner {
    public Alignment alignSentencePair(SentencePair sentencePair) {
       Alignment alignment = new Alignment();
       double[][] gamma = trainAlignmentForSentence(
+            sentencePair.frenchWords, sentencePair.englishWords);
+      
+      //print2dArray(gamma);
+      
+      int numEnglishWords = sentencePair.englishWords.size();
+      int numFrenchWords = sentencePair.frenchWords.size();
+      
+      for (int fi = 0; fi < numFrenchWords; fi++) {
+         // Find the highest gamma value.
+         int bestEnglishPosition = numEnglishWords;
+         double bestGamma = gamma[fi][numEnglishWords];
+         for (int ei = 0; ei < numEnglishWords; ei++) {
+            if (gamma[fi][ei] > bestGamma) {
+               bestEnglishPosition = ei;
+               bestGamma = gamma[fi][ei];
+            }
+         }
+         
+         // Add the most likely position if it is not NULL.
+         if (bestEnglishPosition != numEnglishWords) {
+            alignment.addAlignment(bestEnglishPosition, fi, true);
+         }
+      }
+      
+      return alignment;
+   }
+
+   protected double[][] trainAlignmentForSentence(List<String> frenchWords,
+         List<String> englishWords) {
+      
+      int numEnglishWords = englishWords.size();
+      int numFrenchWords = frenchWords.size();
+      
+      // The +1 is for NULL, i.e. we store NULL as the last English word.
+      double[][] alpha = new double[numFrenchWords][numEnglishWords + 1];
+      double[][] gamma = new double[numFrenchWords][numEnglishWords + 1];
+      
+      // Construct transition matrix.
+      double[][] trans = new double[numEnglishWords + 1][numEnglishWords + 1];
+      for (int i = 0; i < numEnglishWords; i++) {
+         trans[i][numEnglishWords] = nullDistortionLikelihood;
+         trans[numEnglishWords][i] = (1 - nullDistortionLikelihood)
+               / numEnglishWords;
+         double norm = 0;
+         for (int j = 0; j < numEnglishWords; j++) {
+            trans[i][j] = Math.exp(- 2 * Math.abs(j - i - 1.1));
+            norm += trans[i][j];
+         }
+         for (int j = 0; j < numEnglishWords; j++) {
+            trans[i][j] = trans[i][j] * (1 - nullDistortionLikelihood) / norm;
+         }
+      }
+
+      // Get the first French word.
+      String firstFWord = frenchWords.get(0);
+      int firstFWordIdx = frenchWordIndexer.addAndGetIndex(firstFWord);
+      frenchIndexBuffer[0] = firstFWordIdx;
+      
+      // Generate the English word index. Use the last one as NULL
+      // alignment. Also set the initial alpha.
+      double normAlphaZero = 0;
+      for (int ei = 0; ei < numEnglishWords; ei++) {
+         String englishWord = englishWords.get(ei);
+         int eIndex = englishWordIndexer.addAndGetIndex(englishWord);
+         englishIndexBuffer[ei] = eIndex;
+         
+         // Initialize alpha.
+         alpha[0][ei] = pairCounters.getCount(eIndex, firstFWordIdx)
+               * trans[ei][numEnglishWords];
+         //alpha[0][ei] = trans[ei][numEnglishWords];
+         normAlphaZero += alpha[0][ei];
+      }
+      
+      // Initialize the NULL word (and the last alpha[0] value).
+      englishIndexBuffer[numEnglishWords] = -1;
+      alpha[0][numEnglishWords] = pairCounters.getCount(-1, firstFWordIdx)
