798780333c652aac4fe0bbcd9cae03049470ac7f
femboyish <155249604+femboyish@users.noreply.github.com>
2024-01-26 11:46:51 +0000

    New style

--- a3/src/edu/berkeley/nlp/assignments/assign3/student/HmmAligner.java
+++ a3/src/edu/berkeley/nlp/assignments/assign3/student/HmmAligner.java
@@ -110,3 +110,75 @@ public class HmmAligner extends Model1HardEmAligner {
       // Initialize the NULL word (and the last alpha[0] value).
       englishIndexBuffer[numEnglishWords] = -1;
       alpha[0][numEnglishWords] = pairCounters.getCount(-1, firstFWordIdx)
+            * trans[numEnglishWords][numEnglishWords];
+      //alpha[0][numEnglishWords] = trans[numEnglishWords][numEnglishWords];
+      normAlphaZero += alpha[0][numEnglishWords];
+      if (normAlphaZero != 0) {
+         for (int ei = 0; ei <= numEnglishWords; ei++) {
+            alpha[0][ei] /= normAlphaZero;
+         }
+      }
+      
+      // Calculate alpha by going forward.
+      for (int fi = 1; fi < numFrenchWords; fi++) {
+         String fWord = frenchWords.get(fi);
+         int fWordIndex = frenchWordIndexer.addAndGetIndex(fWord);
+         frenchIndexBuffer[fi] = fWordIndex;
+         double norm = 0;
+
+         for (int ei = 0; ei <= numEnglishWords; ei++) {
+            for (int j = 0; j <= numEnglishWords; j++) {
+               alpha[fi][ei] += alpha[fi - 1][j] * trans[j][ei];
+            }
+            alpha[fi][ei] *= pairCounters.getCount(englishIndexBuffer[ei],
+                  fWordIndex);
+            norm += alpha[fi][ei];
+         }
+         
+         // Normalize alpha.
+         if (norm > 0) {
+            for (int ei = 0; ei <= numEnglishWords; ei++) {
+               alpha[fi][ei] /= norm;
+            }
+         }
+      }
+      
+      // Initialize gamma[numFrenchWords - 1].
+      System.arraycopy(alpha[numFrenchWords - 1], 0, gamma[numFrenchWords - 1],
+            0, numEnglishWords);
+      
+      // Used to buffer the normalization factors.
+      double[] normFactors = new double[numEnglishWords + 1];
+      
+      // Calculate gamma by going backward.
+      for (int fi = numFrenchWords - 2; fi >= 0; fi--) {
+         
+         // Calculate the normalization factors.
+         for (int j = 0; j <= numEnglishWords; j++) {
+            for (int j1 = 0; j1 <= numEnglishWords; j1++) {
+               normFactors[j] += alpha[fi][j1] * trans[j1][j];
+            }
+         }
+         
+         // Calculate gammas.
+         for (int ei = 0; ei <= numEnglishWords; ei++) {
+            for (int j = 0; j <= numEnglishWords; j++) {
+               
+               if (normFactors[j] == 0) {
+                  gamma[fi][ei] = 0;
+               } else {
+                  gamma[fi][ei] += alpha[fi][ei] * trans[ei][j]
+                        * gamma[fi + 1][j] / normFactors[j];
+               }
+            }
+         }
+      }
+
+      return gamma;
+   }
+   
+   /* (non-Javadoc)
+    * @see edu.berkeley.nlp.assignments.assign3.student.AlignerBase#train(java.lang.Iterable)
+    */
+   public void train(Iterable<SentencePair> trainingData) {
+      
