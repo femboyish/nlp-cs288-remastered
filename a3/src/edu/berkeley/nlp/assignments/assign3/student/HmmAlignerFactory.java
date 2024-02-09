52f222f1dce7fe85d6ab8289502f31b365856b7f
femboyish <155249604+femboyish@users.noreply.github.com>
2024-02-09 16:07:03 +0000

    Switched broken pypip.in badges to shields.io

--- a3/src/edu/berkeley/nlp/assignments/assign3/student/HmmAlignerFactory.java
+++ a3/src/edu/berkeley/nlp/assignments/assign3/student/HmmAlignerFactory.java
@@ -2,4 +2,6 @@ package edu.berkeley.nlp.assignments.assign3.student;
 
 import edu.berkeley.nlp.mt.SentencePair;
 import edu.berkeley.nlp.mt.WordAligner;
-import edu.berkeley.nlp.mt.WordAligne
+import edu.berkeley.nlp.mt.WordAlignerFactory;
+
+public class HmmAlignerFactory implements WordAlig
