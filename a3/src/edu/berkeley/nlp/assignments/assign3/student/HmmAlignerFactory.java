c64dea262db7b234d62ebdd3fdd27268b644dd70
femboyish <155249604+femboyish@users.noreply.github.com>
2024-02-09 22:38:47 +0000

    updated API

--- a3/src/edu/berkeley/nlp/assignments/assign3/student/HmmAlignerFactory.java
+++ a3/src/edu/berkeley/nlp/assignments/assign3/student/HmmAlignerFactory.java
@@ -4,4 +4,7 @@ import edu.berkeley.nlp.mt.SentencePair;
 import edu.berkeley.nlp.mt.WordAligner;
 import edu.berkeley.nlp.mt.WordAlignerFactory;
 
-public class HmmAlignerFactory implements WordAlig
+public class HmmAlignerFactory implements WordAlignerFactory
+{
+
+	public WordAligner newAligner(Iterable<SentencePair> tr
