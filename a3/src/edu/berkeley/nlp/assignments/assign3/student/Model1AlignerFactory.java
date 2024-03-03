cf5fbf09bbec0c617d3794e5555eb06844d9ad4c
femboyish <155249604+femboyish@users.noreply.github.com>
2024-03-03 20:49:42 +0000

    Add codeclimate

--- a3/src/edu/berkeley/nlp/assignments/assign3/student/Model1AlignerFactory.java
+++ a3/src/edu/berkeley/nlp/assignments/assign3/student/Model1AlignerFactory.java
@@ -2,4 +2,9 @@ package edu.berkeley.nlp.assignments.assign3.student;
 
 import edu.berkeley.nlp.mt.SentencePair;
 import edu.berkeley.nlp.mt.WordAligner;
-import edu.berkeley.nlp.mt.WordAl
+import edu.berkeley.nlp.mt.WordAlignerFactory;
+
+public class Model1AlignerFactory implements WordAlignerFactory
+{
+
+	
